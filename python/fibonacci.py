48b3715a7ed417345101ac1f271a07693fd4ef1d
kayleeburch <kayleemburch@gmail.com>
2022-05-25 15:10:28 -0400

    python solution

--- python/fibonacci.py
+++ python/fibonacci.py
@@ -1,4 +1,12 @@
+# 0 1 1 2 3 5 8 13 21 34 55
 def fibonacci(n):
-  pass
+  firstNum = 0
+  secondNum = 1
+  for i in range(n):
+    adding = firstNum + secondNum
+    firstNum = secondNum
+    secondNum = adding
+    # print(adding, firstNum, secondNum)
+  return firstNum
 
 
